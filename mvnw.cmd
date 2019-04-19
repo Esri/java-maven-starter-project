f54a4b16d419bd80a21a22b2f8da194ef8c4245f
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-04-19 10:45:00 -0700

    add copyright

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,4 +1,20 @@
 @REM ----------------------------------------------------------------------------
+@REM Copyright 2019 Esri
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License"); you may not
+@REM use this file except in compliance with the License. You may obtain a copy
+@REM of the License at
+@REM
+@REM http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+@REM WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+@REM License for the specific language governing permissions and limitations
+@REM under the License.
+@REM ----------------------------------------------------------------------------
+
+@REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
 @REM distributed with this work for additional information
